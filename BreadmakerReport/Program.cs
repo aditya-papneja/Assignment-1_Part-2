664bbed96aa1a971822f97d37cf15e9280e9cfd5
Aditya Papneja <apapneja@confederationcollege.ca>
2021-08-15 23:19:23 -0500

    assignment 1 part 2

---
+++ BreadmakerReport/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using RatingAdjustment.Services;
+using BreadmakerReport.Models;
+
+namespace BreadmakerReport
+{
+    class Program
+    {
+        static string dbfile = @".\data\breadmakers.db";
+        static RatingAdjustmentService ratingAdjustmentService = new RatingAdjustmentService();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Bread World");
+            var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
+            //var BMList = BreadmakerDb.Breadmakers.SelectMany(r => r.Reviews.GroupBy(x => x), (b, r) => new
+            //{
+
+            //    Reviews = b.Reviews.Count(),
+            //    Avarage = b.Reviews.Select(re => re.stars).Average(),
+            //    Description = b.title,
+            //    Adjust = ratingAdjustmentService.Adjust(b.Reviews.Select(s => s.stars).Count(), b.Reviews.Count())
+
+            //}).ToList();
+            var BMList = BreadmakerDb.Breadmakers.SelectMany(r => r.Reviews).Distinct().GroupBy(x => x.BreadmakerId)
+              .Select(r => new {
+                  Reviews = r.Count(),
+                  Avarage = r.Average(x => x.stars),
+                  Adjust = ratingAdjustmentService.Adjust(r.Sum(x => x.stars), r.Count()),
+                  bmId = r.Key
+
+              }).ToList();
+            Console.WriteLine("[#]  Reviews Average  Adjust    Description");
+            for (var j = 0; j < 3; j++)
+            {
+                var i = BMList[j];
+               var br= BreadmakerDb.Breadmakers.FirstOrDefault(b => b.BreadmakerId == i.bmId);
+
+                Console.WriteLine($"{j}1\t{i.Reviews}1\t{i.Avarage.ToString("F1")}1\t{ i.Adjust.ToString("F1")}1\t{br.title}");
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace RatingAdjustment.Services
+{
+    /** Service calculating a star rating accounting for the number of reviews
+     * 
+     */
+    public class RatingAdjustmentService
+    {
+        // TODO: Replace this file with the one from Part A!
+        const double MAX_STARS = 5.0;  // Likert scale
+        const double Z = 1.96; // 95% confidence interval
+
+        double _q;
+        double _percent_positive;
+
+        /** Percentage of positive reviews
+         * 
+         * In this case, that means X of 5 ==> percent positive
+         * 
+         * Returns: [0, 1]
+         */
+        public void SetPercentPositive(double stars)
+        {
+            // TODO: Implement this!
+            _percent_positive = stars * 2 / 10;
+        }
+
+        /**
+         * Calculate "Q" given the formula in the problem statement
+         */
+        public void SetQ(double number_of_ratings)
+        {
+            // TODO: Implement this!
+
+            var n = number_of_ratings;
+
+            // p̂, the fraction of upvotes
+            var phat = _percent_positive;
+
+            _q = (phat + Z * Z / (2 * n) - Z * Math.Sqrt((phat * (1 - phat) + Z * Z / (4 * n)) / n)) / (1 + Z * Z / n);
+        }
+
+
+        /** Adjusted lower bound
+         * 
+         * Lower bound of the confidence interval around the star rating.
+         * 
+         * Returns: a double, up to 5
+         */
+        public double Adjust(double stars, double number_of_ratings)
+        {
+            // TODO: Implement this!
+            var phat = 1.0 * number_of_ratings / stars;
+            var n = number_of_ratings;
+            return (phat + Z * Z / (2 * n) - Z * Math.Sqrt((phat * (1 - phat) + Z * Z / (4 * n)) / n)) / (1 + Z * Z / n);
+        }
+    }
+}
